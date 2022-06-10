5a63375385d3c83ae0d1fcd18a1ce73845b3b4e7
RadwaAdel <radwaadel551@yahoo.com>
2022-06-11 00:26:08 +0200

    jjjj

---
+++ index.js
@@ -0,0 +1,39 @@
+let mongoose = require('mongoose');
+let express = require('express');
+let app = express();
+let product = require('./routes/product')
+let category = require('./routes/category')
+let cart = require('./rootes/cart')
+let user = require('./routes/user')
+let order =require('/routes/order')
+let bcrypt = require('bcryptjs');
+let jwt = require('jsonwebtoken');
+const User = require('./models/user');
+mongoose.connect('mongodb://localhost/ecommerce')
+    .then(console.log('connected'))
+    .catch(err => console.log('err: ' + err));
+
+app.use(express.json());
+
+
+app.post('/login', async(req, res) => {
+    try {
+        let user = await User.findByCradentails(req.body.email, req.body.password);
+        let token = user.generateAuthToken();
+        res.send({ user, token });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+
+
+app.use('/product', product);
+app.use('/category', category);
+app.use('/user', user);
+app.cart('/cart', cart);
+app.order('/order', order);
+
+
+
+app.listen(5000)
