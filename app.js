16a33fea7953441cfb3e9e73c762f04a75a54bfe
Abdelmoamen Gomaa <moamengomaa27@gmail.com>
2022-06-10 21:06:26 +0200

    assmaa

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const productRoute = require('./routes/products')
 const categoryRoute = require("./routes/category");
+let user = require("./routes/user");
+let bcrypt = require("bcryptjs");
+let jwt = require("jsonwebtoken");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -11,8 +14,21 @@ const app = express();
  .then(console.log('connect mongodb to port'))
  .catch(err => console.log(err));
 
+ let User = require("./models/User");
+ mongoose
+   .connect("mongodb://localhost/ecommerce")
+   .then(console.log("connected"))
+   .catch((err) => console.log("err: " + err));
 
-
+app.post("/login", async (req, res) => {
+  try {
+    let user = await User.findByCradentails(req.body.email, req.body.password);
+    let token = user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 
 
@@ -21,6 +37,7 @@ app.use(express.json());
 
 app.use("/product", productRoute);
 app.use("/category", categoryRoute);
+app.use("/user", user);
 
 app.listen(PORT, () => {
     console.log("connect")
